3fe0de6abd64d6a2eb0faae61db794282471538e
Xniu42 <166982610+Xniu42@users.noreply.github.com>
2024-05-28 23:27:15 +0800

    chapater7 fetching data

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,26 +1,41 @@
 import { Revenue } from './definitions';
 
+/**
+ * 将给定的金额数字格式化为美元货币字符串。
+ * @param amount - 代表金额的数字，预期为未格式化的整数（例如，100代表1美元）。
+ * @returns 返回格式化后的美元金额字符串，例如"$1.00"。
+ */
 export const formatCurrency = (amount: number) => {
+  // 将金额除以100，转换为美元格式，并使用toLocaleString方法进行本地化格式化
   return (amount / 100).toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
   });
 };
 
+/**
+ * 将日期字符串格式化为指定地区的本地化日期字符串。
+ * @param dateStr 日期字符串，应遵循ISO 8601日期格式。
+ * @param locale 指定的地区字符串，默认为'en-US'（美国英语）。
+ * @returns 格式化后的本地化日期字符串。
+ */
 export const formatDateToLocal = (
   dateStr: string,
   locale: string = 'en-US',
 ) => {
+  // 创建一个Date对象，使用提供的日期字符串。
   const date = new Date(dateStr);
+  // 定义日期格式化选项，包括日、月和年的表示方式。
   const options: Intl.DateTimeFormatOptions = {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   };
+  // 根据提供的地区和格式化选项创建一个日期格式化器。
   const formatter = new Intl.DateTimeFormat(locale, options);
+  // 使用格式化器将Date对象格式化为指定地区的本地化字符串。
   return formatter.format(date);
 };
-
 export const generateYAxis = (revenue: Revenue[]) => {
   // Calculate what labels we need to display on the y-axis
   // based on highest record and in 1000s
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -42,16 +42,23 @@ export function Card({
   const Icon = iconMap[type];
 
   return (
+      /**
+     * 渲染一个包含标题和值的装饰性组件。
+     * @param Icon 可选，图标组件，用于在标题前展示图标。
+     * @param title 标题文本。
+     * @param value 显示的值文本。
+     * @param lusitana 包含 className 属性的对象，用于自定义最内层 p 元素的样式。
+     */
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null} {/* 如果提供了Icon组件，则显示图标 */}
+        <h3 className="ml-2 text-sm font-medium">{title}</h3> {/* 标题显示区域 */}
       </div>
       <p
         className={`${lusitana.className}
           truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
-        {value}
+        {value} {/* 显示的值 */}
       </p>
     </div>
   );
